ddad4fbbe3c91f9c3e632ed41b8d9f59e802d145
Ajit Pandey <ajitpandey4572@gmail.com>
2026-06-23 12:07:56 +0530

    Final Commit in new repo

---
+++ prac-endterm/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [num, setNum] = useState(0);
+  const [fact, setFact] = useState(0);
+
+  const factorial = () => {
+    let f = 1;
+    for (let i = 1; i <= num; i++) f *= i;
+    setFact(f);
+  };
+
+  return (
+    <>
+      <h1>hello</h1>
+      <div className="card">
+        <h2>Factorial</h2>
+
+        <input
+          type="number"
+          onChange={(e) => setNum(e.target.value)}
+          placeholder="Enter number"
+        />
+
+        <button onClick={factorial}>Find</button>
+
+        <p>{fact}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
